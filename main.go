6079a520c75ff808bb85cae850df342362be430f
notastack <103752556+notastack@users.noreply.github.com>
2022-06-06 21:23:59 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	bills := []int{10000, 500, 200, 100, 50, 10, 5, 2, 1}
+	var usrplay int
+	fmt.Println("How much money you got?")
+	fmt.Scanln(&usrplay)
+	var i int
+	i = 0
+	var nbbills int
+	fmt.Println("That's")
+	for usrplay > 0 {
+		nbbills = (usrplay / bills[i])
+		if nbbills != 0 {
+			fmt.Println(nbbills, " $", bills[i], " bills")
+		}
+		usrplay = usrplay - (nbbills * bills[i])
+		i++
+
+	}
+}
